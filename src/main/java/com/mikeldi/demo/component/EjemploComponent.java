bacfb9a1cb483502e0229059554823e727c1c5ee
jcarrascal-mikeldi <jcarrascal@p.mikeldi.com>
2025-10-10 10:00:25 +0200

    ejemploComponent

---
+++ src/main/java/com/mikeldi/demo/component/EjemploComponent.java
@@ -0,0 +1,22 @@
+package com.mikeldi.demo.component;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+import com.mikeldi.demo.model.Persona;
+
+@Component("ejemploComponent")
+public class EjemploComponent {
+	
+	public ArrayList <Persona> getPersonas(){
+		ArrayList<Persona> listaPersonas=new ArrayList<Persona>();
+		listaPersonas.add(new Persona ("Maria", "perez", "4743747" , 10));
+		listaPersonas.add(new Persona ("Leire", "Gutierrez", "374847438", 27));
+		listaPersonas.add(new Persona ("Lorea", "Bazterra", "74873837Y" , 30));
+		listaPersonas.add(new Persona ("Elena", "Dominguez", "333222333" , 35));
+		listaPersonas.add(new Persona ("Ander", "Díez", "88837487", 25));
+		return listaPersonas;
+	}
+
+}
--- src/main/java/com/mikeldi/demo/controller/EjemploFormularioListadoController.java
+++ src/main/java/com/mikeldi/demo/controller/EjemploFormularioListadoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mikeldi.demo.component.EjemploComponent;
 import com.mikeldi.demo.model.Persona;
 
 @Controller
@@ -22,11 +25,15 @@ public class EjemploFormularioListadoController {
 	final String plantillaFormularioListado = "formulariolistado";
 	private ArrayList<Persona> personas= new ArrayList<Persona>();
 	private static final Log LOG = LogFactory.getLog(EjemploFormularioListadoController.class);
+	
+	@Autowired 
+	@Qualifier("ejemploComponent") 
+	EjemploComponent ejemplo;
 
 	@GetMapping("/mostrarformulario")
 	public String mostrarForm(Model model) {
 		model.addAttribute("persona", new Persona());
-		personas.add(new Persona("Pedro", "Perez", "0", 22));
+		personas= ejemplo.getPersonas();
 		model.addAttribute("personas", personas);
 		return plantillaFormularioListado;
 	}
@@ -38,6 +45,7 @@ public class EjemploFormularioListadoController {
 		personas.add(persona);
 		LOG.info("Añadida persona " + persona.getNombre() + persona.getApellidos());
 		mav.addObject("personas", personas);
+		mav.addObject("persona", new Persona());
 		return mav;
 	}
 	
